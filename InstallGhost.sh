ae8374b6acd10443e5cbe5a43ab592630572da43
Wu Song <ryanws@foxmail.com>
2017-02-06 13:01:11 +0800

    Install a ghost blog by yourself

---
+++ InstallGhost.sh
@@ -0,0 +1,58 @@
+# www.bayun.org
+clear
+
+echo ""
+
+# make sure if it is run by root
+if [ `whoami` != root ]; then
+    echo "This script must be run as root!"
+    exit 0
+fi
+
+# Input the hostname of Ubuntu
+echo "This script install Ghost on Ubuntu"
+echo "Please provide your hostname(eg:www.bayun.org):"
+echo ""
+read HOSTNAME
+
+# Update and install some commands
+apt-get update -y
+apt-get install -y curl unzip vim wget screen
+
+# Download the latest version of Ghost
+cd /home
+wget http://sosososo.oss-cn-beijing.aliyuncs.com/software/ghost-latest.zip
+unzip -d ghost ghost-latest.zip
+rm -rf ghost-latest.zip
+cd ghost
+sed -e "s/my-ghost-blog.com/$HOSTNAME/" <config.example.js >config.js
+
+# Use cnpm instead of npm
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt-get install -y nodejs
+apt-get install -y npm
+ln -s /usr/bin/nodejs /usr/bin/node
+npm install -g cnpm --registry=https://registry.npm.taobao.org
+cnpm install
+
+# Install Nginx
+apt-get install -y nginx
+rm -rf /etc/nginx/sites-enabled/default
+
+echo "Configuring Nginx"
+echo "server {" >> /etc/nginx/sites-available/ghost
+echo "    listen 80;" >> /etc/nginx/sites-available/ghost
+echo "    server_name $HOSTNAME;" >> /etc/nginx/sites-available/ghost
+echo "    location / {" >> /etc/nginx/conf.d/ghost.conf
+echo "        proxy_set_header   X-Real-IP \$remote_addr;" >> /etc/nginx/sites-available/ghost
+echo "        proxy_set_header   Host      \$http_host;" >> /etc/nginx/sites-available/ghost
+echo "        proxy_pass         http://127.0.0.1:2368;" >> /etc/nginx/sites-available/ghost
+echo "        }" >> /etc/nginx/sites-available/ghost
+echo "    }" >> /etc/nginx/sites-available/ghost
+
+sudo ln -s /etc/nginx/sites-available/ghost /etc/nginx/sites-enabled/ghost
+service nginx restart
+
+# Start Ghost
+cd /home/ghost
+npm start --production
